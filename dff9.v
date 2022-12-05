9151e62082443b0c7bf248f6e27904af1376b8df
GTPV <parangvo@gmail.com>
2022-12-05 19:50:02 +0900

    dff merged : dff+dff8 => dff9

--- dff9.v
+++ dff9.v
@@ -1,18 +1,22 @@
-//dff8.v
+//dff9.v
 `timescale 1ns / 1ps
 
-module dff8(
+module dff9(
     input clk,
     input reset_n,
-    input [7:0] D,
-    output reg [7:0] Q
+    input D1,
+    input [7:0] D8,
+    output reg Q1,
+    output reg [7:0] Q8
 );
     always @(posedge clk or negedge reset_n) begin
         if(reset_n == 0) begin
-            Q <= 8'b00000000;
+            Q1 <= 1'b0;
+            Q8 <= 8'b00000000;
         end
         else begin
-            Q <= D;
+            Q1 <= D1;
+            Q8 <= D8;
         end
     end
 endmodule
